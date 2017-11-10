011497d7f999d51458f93949354f41e47772df54
HyunHo Noh <geonspapa@gmail.com>
2017-11-10 11:28:05 +0900

    Create show_images.js

---
+++ show_images.js
@@ -0,0 +1,24 @@
+require([
+    'underscore',
+    'jquery',
+    'splunkjs/mvc',
+    'splunkjs/mvc/tableview',
+    'splunkjs/mvc/simplexml/ready!'
+], function(_, $, mvc, TableView) {
+    
+    var Imgsrc = TableView.BaseCellRenderer.extend({
+        canRender: function(cell) {
+            // Only use the cell renderer for the range field
+            return cell.field === 'IMAGE';
+        },
+        render: function($td, cell) {
+            $td.html(_.template('<img src="<%-imgsrc%>" height="100"/>',{imgsrc:cell.value}));
+        }
+    });
+    mvc.Components.get('table1').getVisualization(function(tableView){
+        // Register custom cell renderer
+        tableView.table.addCellRenderer(new Imgsrc());
+        // Force the table to re-render
+        tableView.table.render();
+    });
+});
